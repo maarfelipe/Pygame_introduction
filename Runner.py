065b92f38e7c444712671c7bb1facfaba54882a9
Matheus Felipe <matheus.maarfe@hotmail.com>
2022-05-24 16:49:22 -0300

    add imagens e superficies

--- Runner.py
+++ Runner.py
@@ -5,13 +5,21 @@ pygame.init()
 screen = pygame.display.set_mode((800, 400))
 pygame.display.set_caption('Runner')
 clock = pygame.time.Clock()
+test_font = pygame.font.Font('font/Pixeltype.ttf', 50)
+
+sky_surface = pygame.image.load('graphics/sky.png')
+ground_surface = pygame.image.load('graphics/ground.png')
+test_surface = test_font.render('My Game', False, 'Black')
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
-    # draw all out elements
-    # update everything
+
+    screen.blit(sky_surface, (0, 0))
+    screen.blit(ground_surface, (0, 300))
+    screen.blit(test_surface, (300, 50))
+
     pygame.display.update()
     clock.tick(60)
