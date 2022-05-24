00524d2904fa4373d303e2635170843c313b06bf
Matheus Felipe <matheus.maarfe@hotmail.com>
2022-05-24 17:07:24 -0300

    adicionando animacoes basicas

--- Runner.py
+++ Runner.py
@@ -7,9 +7,12 @@ pygame.display.set_caption('Runner')
 clock = pygame.time.Clock()
 test_font = pygame.font.Font('font/Pixeltype.ttf', 50)
 
-sky_surface = pygame.image.load('graphics/sky.png')
-ground_surface = pygame.image.load('graphics/ground.png')
-test_surface = test_font.render('My Game', False, 'Black')
+sky_surface = pygame.image.load('graphics/sky.png').convert()
+ground_surface = pygame.image.load('graphics/ground.png').convert()
+text_surface = test_font.render('My Game', False, 'Black')
+
+snail_surface = pygame.image.load('graphics/snail/snail1.png').convert_alpha()
+snail_x_pos = 600
 
 while True:
     for event in pygame.event.get():
@@ -19,7 +22,11 @@ while True:
 
     screen.blit(sky_surface, (0, 0))
     screen.blit(ground_surface, (0, 300))
-    screen.blit(test_surface, (300, 50))
+    screen.blit(text_surface, (300, 50))
+    snail_x_pos -= 4
+    if snail_x_pos < -1000:
+        snail_x_pos = 800
+    screen.blit(snail_surface, (snail_x_pos, 250))
 
     pygame.display.update()
     clock.tick(60)
