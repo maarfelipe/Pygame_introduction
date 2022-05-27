297c390b0feb5e2d80c34b4938eb12cb78685e76
Matheus Felipe <matheus.maarfe@hotmail.com>
2022-05-27 15:22:01 -0300

    add comentários para estudos posteriores

--- Runner.py
+++ Runner.py
@@ -1,36 +1,83 @@
 import pygame
 from sys import exit
 
+# comando para iniciar o pygame e todas as suas funções
 pygame.init()
+# define o display surface, a tela que será apresentada ao usuário (width, height)
 screen = pygame.display.set_mode((800, 400))
+# define o título apresentado na janela
 pygame.display.set_caption('Runner')
+# define um objeto clock, utilizado para controle de framerate (é preciso ser "chamado" posteriormente)
 clock = pygame.time.Clock()
+# define um estilo de font que pode ser importado, o seu tamanho também é definido agora
 test_font = pygame.font.Font('font/Pixeltype.ttf', 50)
 
+# # dentro de uma variável, é possível guardar o tamanho de uma superfície e posteriormente "enchê-la" com uma cor
+# test_surface = pygame.Surface((100, 200))
+# test_surface.fill('Red')
+
+# é possível também importar uma imagem por exemplo,
+# é necessário utilizar o .convert() para facilitar o "entendimento" do pygame ao que está sendo importado
 sky_surface = pygame.image.load('graphics/sky.png').convert()
 ground_surface = pygame.image.load('graphics/ground.png').convert()
+# cria um objeto que acessa o estilo previamente criado e recebe 3 parâmetros:
+# o texto que será exibido, boolean para filtro anti-aliasing e cor do texto
 text_surface = test_font.render('My Game', False, 'Black')
 
+# para objetos que vão se mover ou receber algum tipo de animação, é recomendado a utilização de .convert_aplha()
 snail_surf = pygame.image.load('graphics/snail/snail1.png').convert_alpha()
+# cria uma variável que recebe o objeto previamente importado junto com as medidas de um retângulo
+# os valores entre parênteses definem qual é a posição na tela daquele ponto do retângulo escolhido
 snail_rect = snail_surf.get_rect(bottomright=(600, 300))
 
 player_surf = pygame.image.load('graphics/Player/player_walk_1.png').convert_alpha()
+"""para parametrizar os retângulos, podemos acessar os valores de tudas formas:
+    tuplas(x, y):
+        podemos acessar cada valor separado em nove possíveis posições:
+            topleft         midtop          topright
+            midleft         center          midright
+            bottomleft      midbottoom      bottomright
+    valores individuais:
+        pode-se acessar os valores de um lado inteiro individualmente:
+                            top
+            left    centerx OU centery  right
+                            bottom
+"""
 player_rect = player_surf.get_rect(midbottom=(80, 300))
 
+# o loop é criado para que a tela e os eventos permaneçam acontecendo, senão, tudo iria ser executado somente uma vez
 while True:
     for event in pygame.event.get():
+        # quando o usuário clicar no X para fechar a janela, o loop vai entender
         if event.type == pygame.QUIT:
+            # pygame.quit() é literalmente o oposto de pygame.init()
+            # permitindo que tudo seja encerrado dá forma correta sem apresentar nenhum tipo de erro
             pygame.quit()
+            # importado do sys, exit() encerra e fecha a janela da forma correta
             exit()
 
+    # # aqui estamos acessando a variável da superfície vermelha e escolhendo qual a sua posição
+    # screen.blit(test_surface, (0, 0))
+    # acessando o objeto sky_surface que foi previamente carregado fora do loop
     screen.blit(sky_surface, (0, 0))
+    # os valores entre parênteses definem a posição da imagem na tela criada,
+    # a ordem de chamada do screen.blit influencia na posição das imagens (o que fica na frente do que)
     screen.blit(ground_surface, (0, 300))
     screen.blit(text_surface, (300, 50))
 
-    snail_rect.x -= 4
-    if snail_rect.right < 0: snail_rect.left = 800
+    # posiciona na tela o objeto snail_surf posição snail_rect (que foi o retângulo previamente criado)
     screen.blit(snail_surf, snail_rect)
+    # acessando o eixo X do retângulo para definir sua motimentação na tela
+    snail_rect.x -= 4
+    # se a posição da direita do retângulo for menor que 0 no eixo X
+    if snail_rect.right < 0:
+        # a posição esquerda do retângulo no eixo X é definida para 800
+        snail_rect.left = 800
     screen.blit(player_surf, player_rect)
 
+    # este comando atualiza frame por frame o que está acontecendo dentro do loop,
+    # é ele que mostra para o usuário a animação acontecendo
     pygame.display.update()
+    # o valor dentro dos parênteses indica quantas repetições por segundo acontecerão
+    # no caso, 60fps ... o loop vai se repetir 60 vezes por segundo
     clock.tick(60)
